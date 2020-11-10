9212f239b7b55910af1a25582f5e131e4df9837c
Jose Miller <jomicasierra@unac.edu.co>
2020-11-09 20:20:32 -0500

    refactor , solucion de error submmit

--- src/test/java/stepdefinitions/RegistroStepdefinitions.java
+++ src/test/java/stepdefinitions/RegistroStepdefinitions.java
@@ -1,49 +1,62 @@
 package stepdefinitions;
 
+import java.util.concurrent.TimeUnit;
+
 import org.junit.Assert;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Wait;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import io.cucumber.messages.Messages.Duration;
 
 public class RegistroStepdefinitions {
 	
 	 WebDriver driver;
+	 JavascriptExecutor js = (JavascriptExecutor) driver;
 
 	@Given("El usuario esta en la pagina de registro")
 	public void abrirPaginaDeInicioa() {
 		System.setProperty("webdriver.chrome.driver","C:\\chromedriver.exe");
 		driver= new ChromeDriver();
+		driver.manage().window().maximize();
 		driver.get("http://demo.guru99.com/telecom/addcustomer.php");
+		WebDriverWait wait=new WebDriverWait(driver, 20);
 	 
 	}
 	
 	@When("Cuando ingresa datos validos")
 	public void cuando_ingresa_datos_validos() {
-		driver.findElement(By.xpath("//label[contains(text(),'Done')]")).click();
-		driver.findElement(By.xpath("//input[@id='fname']")).sendKeys("Milner");
-		driver.findElement(By.xpath("//input[@id='lname']")).sendKeys("Bond");
-		driver.findElement(By.xpath("//input[@id='email']")).sendKeys("jalbak65@gmail.com");
-		driver.findElement(By.xpath("//textarea[@id='message']")).sendKeys("Esta es mi direccion");
-		driver.findElement(By.xpath("//input[@id='telephoneno']")).sendKeys("343434556");
+	    driver.findElement(By.cssSelector(".\\32u:nth-child(1) > label")).click();
+	    driver.findElement(By.id("fname")).sendKeys("miller");
+	    driver.findElement(By.id("lname")).sendKeys("casierr");
+	    driver.findElement(By.id("email")).sendKeys("elbichocr714@gmail.com");
+	    driver.findElement(By.name("addr")).sendKeys("mi direccion");
+	    driver.findElement(By.id("telephoneno")).sendKeys("1212121212");
 
 	}
 
 
 	@When("da clic en submmit")
-	public void da_clic_en_submmit() {
-		driver.findElement(By.cssSelector("#main > div > form > div > div:nth-child(9) > ul > li:nth-child(1) > input[type=submit]")).click();
+	public void da_clic_en_submmit() throws InterruptedException {
+		WebElement ele = driver.findElement(By.name("submit"));
+		JavascriptExecutor jse = (JavascriptExecutor)driver;
+		jse.executeScript("arguments[0].click()", ele);
+		
 	  
 	}
 
 
 	@Then("El usuario puede ver que su registro halla sido exitoso")
-	public void elUsuarioPuedeVerSuPanelDeAdministracion() throws InterruptedException {
+	public void elUsuarioPuedeVerSuPanelDeAdministracion() {
 		Assert.assertEquals(driver.findElement(By.xpath("//h1[contains(text(),'Access Details to Guru99 Telecom')]")).getText(), "Access Details to Guru99 Telecom");
-		Thread.sleep(30000);
 		driver.quit();
 
 	}
